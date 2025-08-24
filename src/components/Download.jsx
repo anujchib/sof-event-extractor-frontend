bed1edc073baf782f33472a6e8c4e403311d4bf2
anujchib <anujchibofficial0003@gmail.com>
2025-08-24 16:20:03 +0530

    download-fix

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -18,7 +18,7 @@ const Download =()=>{
 
         try {
 
-          const upload =  await fetch('https://localhost:3000/upload-url',{
+          const upload =  await fetch('http://localhost:3000/upload-url',{
                 method:'POST',
                 headers:{
                     'Content-Type':'application/json'
