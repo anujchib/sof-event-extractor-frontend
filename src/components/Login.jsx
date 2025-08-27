17020c0242597ab0cb38364467de81ea891261a9
anujchib <anujchibofficial0003@gmail.com>
2025-08-27 09:24:17 +0530

    ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,19 +15,19 @@ const Login = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-white px-6">
       <div className="text-center max-w-md">
-        <h1 className="font-serif font-bold text-green-700 text-3xl mb-4">
+        <h1 className="font-serif font-bold text-black text-shadow-blue-600 text-3xl mb-4">
           Sof-event-extractor&reg;
         </h1>
 
         {/* Cute note for users */}
         <p className="text-gray-600 mb-8 font-medium">
           Hey there 👋 I’m a student and this app costs money to run.
-          Please don’t use it heavily, or it’ll exhaust my credits 🙏
+          Please don’t use it heavily, or it’ll exhaust my credits .
         </p>
 
         {/* Google login button */}
         <a
-          className="px-8 py-3 rounded-lg bg-green-600 text-white font-semibold shadow hover:bg-green-700 transition"
+          className="px-8 py-3 rounded-lg bg-gradient-to-r from-blue-200 to-blue-700 text-white font-semibold shadow hover:bg-blue-500 transition"
           href="https://mastering-auth-production-c0d1.up.railway.app/api/v1/user/auth/google"
         >
           Login with Google
