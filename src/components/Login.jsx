286d09ee7a2b37caaa805b0d59336151a1560506
anujchib <anujchibofficial0003@gmail.com>
2025-08-27 09:40:26 +0530

    ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,9 +13,9 @@ const Login = () => {
   }, [navigate]);
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-white px-6">
-      <div className="text-center max-w-md">
-        <h1 className="font-serif font-bold text-black text-shadow-blue-600 text-3xl mb-4">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-200 px-6">
+      <div className="text-center max-w-md  border-dotted rounded-4xl border-black p-14 border-r-orange-600 border-l-blue-800 border-2">
+        <h1 className="font-sans font-bold text-black text-shadow-blue-600 text-3xl mb-4">
           Sof-event-extractor&reg;
         </h1>
 
@@ -27,12 +27,15 @@ const Login = () => {
 
         {/* Google login button */}
         <a
-          className="px-8 py-3 rounded-lg bg-gradient-to-r from-blue-200 to-blue-700 text-white font-semibold shadow hover:bg-blue-500 transition"
+          className="px-8 py-3 rounded-lg bg-gradient-to-r from-blue-400 to-blue-900 text-white text-shadow-amber-800 font-semibold shadow hover:bg-amber-600 transition"
           href="https://mastering-auth-production-c0d1.up.railway.app/api/v1/user/auth/google"
         >
           Login with Google
         </a>
       </div>
+      <footer className="m-10 text-gray-500">
+        <h1 className=" text-sm md:text-xl"> ⚠️This is testing user interface !</h1>
+      </footer>
     </div>
   );
 };
